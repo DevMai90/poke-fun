d577ab590132033b0a5fc8700a1d233d412a0a2d
DevMai90 <dmai90@yahoo.com>
2020-07-19 22:15:35 -0700

    Update pokemon response

--- app/routes/pokemon.js
+++ app/routes/pokemon.js
@@ -4,44 +4,99 @@ export default Route.extend({
   // Model hook gets called instantly.
   // Data becomes available through this.model in the template
   model() {
-    const response = {
-      form_name: '',
-      form_names: [],
-      form_order: 1,
-      id: 150,
-      is_battle_only: false,
-      is_default: true,
-      is_mega: false,
-      name: 'mewtwo',
-      names: [],
-      order: 231,
-      pokemon: {
-        name: 'mewtwo',
-        url: 'https://pokeapi.co/api/v2/pokemon/150/'
+    const response = [
+      {
+        form_name: '',
+        form_names: [],
+        form_order: 1,
+        id: 4,
+        is_battle_only: false,
+        is_default: true,
+        is_mega: false,
+        name: 'charmander',
+        names: [],
+        order: 5,
+        pokemon: {
+          name: 'charmander',
+          url: 'https://pokeapi.co/api/v2/pokemon/4/'
+        },
+        sprites: {
+          back_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png',
+          back_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/4.png',
+          front_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+          front_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png'
+        },
+        version_group: {
+          name: 'red-blue',
+          url: 'https://pokeapi.co/api/v2/version-group/1/'
+        }
       },
-      sprites: {
-        back_default:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/150.png',
-        back_shiny:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/150.png',
-        front_default:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png',
-        front_shiny:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/150.png'
+      {
+        form_name: '',
+        form_names: [],
+        form_order: 1,
+        id: 5,
+        is_battle_only: false,
+        is_default: true,
+        is_mega: false,
+        name: 'charmeleon',
+        names: [],
+        order: 6,
+        pokemon: {
+          name: 'charmeleon',
+          url: 'https://pokeapi.co/api/v2/pokemon/5/'
+        },
+        sprites: {
+          back_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png',
+          back_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/5.png',
+          front_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
+          front_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/5.png'
+        },
+        version_group: {
+          name: 'red-blue',
+          url: 'https://pokeapi.co/api/v2/version-group/1/'
+        }
       },
-      version_group: {
-        name: 'red-blue',
-        url: 'https://pokeapi.co/api/v2/version-group/1/'
+      {
+        form_name: '',
+        form_names: [],
+        form_order: 1,
+        id: 6,
+        is_battle_only: false,
+        is_default: true,
+        is_mega: false,
+        name: 'charizard',
+        names: [],
+        order: 7,
+        pokemon: {
+          name: 'charizard',
+          url: 'https://pokeapi.co/api/v2/pokemon/6/'
+        },
+        sprites: {
+          back_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png',
+          back_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/6.png',
+          front_default:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+          front_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png'
+        },
+        version_group: {
+          name: 'red-blue',
+          url: 'https://pokeapi.co/api/v2/version-group/1/'
+        }
       }
-    };
+    ];
 
-    const { name, id, sprites, version_group } = response;
-
-    return {
-      name,
-      id,
-      sprites,
-      version_group
-    };
+    return response;
   }
 });
