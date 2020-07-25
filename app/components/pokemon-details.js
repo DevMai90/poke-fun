548caa4cb997881ff74902e5fd555a5f3687a813
DevMai90 <dmai90@yahoo.com>
2020-07-24 23:36:44 -0700

    Add action helper hash and dynamic class name

--- app/components/pokemon-details.js
+++ app/components/pokemon-details.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isWide: false,
+  conditionalClassName: true,
+  actions: {
+    toggleImageSize() {
+      // Toggle property is native to Ember
+      this.toggleProperty('isWide');
+      this.toggleProperty('conditionalClassName');
+    }
+  }
 });
