8903dd20c6def4c171c55c8744bb1c90c2924204
DevMai90 <dmai90@yahoo.com>
2020-07-25 21:36:19 -0700

    Integration test for customer helper function

--- tests/integration/helpers/is-this-charizard-test.js
+++ tests/integration/helpers/is-this-charizard-test.js
@@ -3,15 +3,32 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Helper | is-this-charizard', function(hooks) {
+module('Integration | Helper | is-this-charizard', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('It renders correctly for Charizard', async function (assert) {
+    this.set('inputValue', 'charizard');
+
+    // Pass in the helper function to the hbs template
+    await render(hbs`{{is-this-charizard inputValue}}`);
+
+    // this.element refers to the hbs template that we are rendering. Not necessarily the entire DOM.
+    assert.equal(
+      this.element.textContent.trim(),
+      'ISSA CHARIZARD!!!!!',
+      '1st edition WOTC!!'
+    );
+  });
+
+  test('It renders correctly for non-Charizard', async function (assert) {
+    this.set('inputValue', 'LOL');
 
     await render(hbs`{{is-this-charizard inputValue}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(
+      this.element.textContent.trim(),
+      'Is not a Charizard...',
+      'Boohoo, not a Charizard'
+    );
   });
 });
