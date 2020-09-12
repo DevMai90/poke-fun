57c8dc34ba631f6294c4807ce7bbf9e21c0ae354
DevMai90 <dmai90@yahoo.com>
2020-09-12 15:18:46 -0700

    Initialize routes

--- app/router.js
+++ app/router.js
@@ -7,9 +7,14 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function () {
+  this.route('home', { path: '/ ' });
   this.route('about', { path: '/boogers' }); // Tells Ember router to run about.js route file. Also renders the about hbs file
   this.route('contact', { path: '/contact' });
   this.route('pokemon', { path: '/digimon' });
+  this.route('generations', { path: '/generations' });
+  this.route('games', { path: 'games' });
+  this.route('search', { path: '/search' });
+  this.route('pokedex', { path: '/pokedex' });
 });
 
 export default Router;
