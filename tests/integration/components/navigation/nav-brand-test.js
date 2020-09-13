109cde14cbf61ae906b43f1151c5e10cc23d900f
DevMai90 <dmai90@yahoo.com>
2020-09-12 23:36:44 -0700

    Update integration test for nav-brand component

--- tests/integration/components/navigation/nav-brand-test.js
+++ tests/integration/components/navigation/nav-brand-test.js
@@ -3,24 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | navigation/nav-brand', function(hooks) {
+module('Integration | Component | navigation/nav-brand', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders', async function (assert) {
+    assert.expect(1);
 
     await render(hbs`{{navigation/nav-brand}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#navigation/nav-brand}}
-        template block text
-      {{/navigation/nav-brand}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.navbar-brand').hasText('Pokemon Fun');
   });
 });
