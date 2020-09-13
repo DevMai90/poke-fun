e4bc97627e519f75a9c8891a77a03cbe4b874006
DevMai90 <dmai90@yahoo.com>
2020-09-12 17:39:59 -0700

    Move navigation bar into its own component

--- app/router.js
+++ app/router.js
@@ -7,12 +7,12 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function () {
-  this.route('home', { path: '/ ' });
+  this.route('home', { path: '/home' });
   this.route('about', { path: '/boogers' }); // Tells Ember router to run about.js route file. Also renders the about hbs file
   this.route('contact', { path: '/contact' });
   this.route('pokemon', { path: '/digimon' });
   this.route('generations', { path: '/generations' });
-  this.route('games', { path: 'games' });
+  this.route('games', { path: '/games' });
   this.route('search', { path: '/search' });
   this.route('pokedex', { path: '/pokedex' });
 });
