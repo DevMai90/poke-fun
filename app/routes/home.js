95c0248c6bf7ae0912c22e5bbaad1311a7d0aff4
DevMai90 <dmai90@yahoo.com>
2020-10-11 16:16:39 -0700

    Generate random id

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,21 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 
+/**
+ * 802 unique Pokemon as of Gen 7
+ * Gen 8 Pokemon in process of being added to API
+ * https://github.com/PokeAPI/pokeapi/issues/520
+ */
+const TOTAL_NUMBER_OF_POKEMON = 802;
+
 export default Route.extend({
 
+  randomPokemonId (totalPokemon) {
+    return Math.floor(Math.random() * totalPokemon) +1;
+  },
+
   retrievePokemon: async function() {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/charmander');
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.randomPokemonId(TOTAL_NUMBER_OF_POKEMON)}`);
     const data = await res.json();
 
     return data;
