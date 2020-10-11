790d1ebe2d463abaeae5f3af8e3c3d1d2669bfab
DevMai90 <dmai90@yahoo.com>
2020-10-11 15:27:33 -0700

    Configure Mirage to be disabled by default and add fetch call to home route

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,3 +1,20 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
-export default Route.extend({});
+export default Route.extend({
+
+  retrievePokemon: async function() {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/charmander');
+    const data = await res.json();
+
+    return data;
+  },
+  
+  model() {
+    const randomPokemon = this.retrievePokemon();
+    
+    return RSVP.hash({
+      randomPokemon
+    })
+  }
+});
